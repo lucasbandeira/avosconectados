931a1d4761529063b684f60a2c1e10f5ae030f09
Lucas Bandeira <lucasbandeirasousa@gmail.com>
2020-09-04 21:36:25 -0300

    Correção no texto

--- src/components/banner.js
+++ src/components/banner.js
@@ -11,7 +11,7 @@ export default function BannerMain() {
             O que é o projeto?
           </ContentAreaContainer.Title>
           <ContentAreaContainer.Description>
-            Nosso objetivo é tornar tornar o mundo digital mais acessível.<br/>
+            Nosso objetivo é tornar o mundo digital mais acessível.<br/>
             Nós,<a href="http://www.focolares.org.br/movimento-dos-focolares/um-povo/jovens-por-um-mundo-unido/"><strong> Jovens por um Mundo Unido</strong></a>, durante a pandemia descobrimos que podemos ajudar outras pessoas sem sair de casa.
             Se você precisa de ajuda para utilizar o whatsapp, zoom, skype, google meet, redes sociais e outros aplicativos, então acho que podemos te ajudar!<br/>
             Acesse abaixo nossos tutoriais!
