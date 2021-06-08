da985361e552672298206195ac5ca96cf8024797
Lucas Bandeira <lucasbandeirasousa@gmail.com>
2021-06-08 20:56:45 -0300

    remove link inscricao

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle, linkSubscribe }) => (
+const Header = ({ siteTitle }) => (
   <header
     style={{
       background: 'orange',
@@ -27,27 +27,6 @@ const Header = ({ siteTitle, linkSubscribe }) => (
           {siteTitle}
         </Link>
       </h1>
-    </div>
-    <div
-      style={{
-        position: `absolute`,
-        margin: `0 auto`,
-        right: 200,
-        top: 0,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}>
-        <h1 style={{ margin: 0 }}>
-          <Link
-            to="https://docs.google.com/forms/d/e/1FAIpQLScuAFRaABYlgojSyNLPP1OhlTmj1n34KdCBi8l4Felyj84bMw/viewform"
-            style={{
-              color: `white`,
-              textDecoration: `none`,
-            }}
-          >
-            {linkSubscribe}
-          </Link>
-        </h1>
     </div>
   </header>
 )
